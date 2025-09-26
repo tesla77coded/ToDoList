75763d71ec90db19e2df2afd2d6a85206f62c786
tesla77coded <kadamjaydeep60@gmail.com>
2025-09-26 15:02:23 +0530

    chore: setup swagger.js and mount Swagger UI

--- app.js
+++ app.js
@@ -3,14 +3,22 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
 import taskRoutes from './routes/taskRouter.js';
 import { notFound, errorHandler } from './middleware/errorHandler.js';
+import { swaggerUiMiddleware } from './swagger.js';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 dotenv.config();
+
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/', (req, res) => res.send('API is running...'));
+
+swaggerUiMiddleware(app);
+const spec = YAML.load('./docs/openapi.yaml');
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(spec));
 
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/tasks', taskRoutes);
